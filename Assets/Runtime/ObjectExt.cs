139f56c5414153f736c55e13d889f9bbcdb04dee
Jake Chrumka <jake@recroom.com>
2022-04-01 16:57:51 -0400

    Adding extension methods without CancellationTokens to Object for the Scheduler

--- Assets/Runtime/ObjectExt.cs
+++ Assets/Runtime/ObjectExt.cs
@@ -10,20 +10,40 @@ namespace Fizz6.Scheduler
         {
             return Scheduler.Instance.WaitUntilUpdate(cancellationToken, context);
         }
+
+        public static Task WaitUntilUpdate(this Object context)
+        {
+            return Scheduler.Instance.WaitUntilUpdate(default, context);
+        }
         
         public static Task WaitUntilUpdate(this Object context, int frames, CancellationToken cancellationToken)
         {
             return Scheduler.Instance.WaitUntilUpdate(frames, cancellationToken, context);
         }
         
+        public static Task WaitUntilUpdate(this Object context, int frames)
+        {
+            return Scheduler.Instance.WaitUntilUpdate(frames, default, context);
+        }
+        
         public static Task WaitUntilUpdate(this Object context, float delay, CancellationToken cancellationToken)
         {
             return Scheduler.Instance.WaitUntilUpdate(delay, cancellationToken, context);
+        }
+        
+        public static Task WaitUntilUpdate(this Object context, float delay)
+        {
+            return Scheduler.Instance.WaitUntilUpdate(delay, default, context);
         }
 
         public static Task WaitUntilLateUpdate(this Object context, CancellationToken cancellationToken)
         {
             return Scheduler.Instance.WaitUntilLateUpdate(cancellationToken, context);
+        }
+
+        public static Task WaitUntilLateUpdate(this Object context)
+        {
+            return Scheduler.Instance.WaitUntilLateUpdate(default, context);
         }
         
         public static Task WaitUntilLateUpdate(this Object context, int frames, CancellationToken cancellationToken)
@@ -31,14 +51,29 @@ namespace Fizz6.Scheduler
             return Scheduler.Instance.WaitUntilLateUpdate(frames, cancellationToken, context);
         }
         
+        public static Task WaitUntilLateUpdate(this Object context, int frames)
+        {
+            return Scheduler.Instance.WaitUntilLateUpdate(frames, default, context);
+        }
+        
         public static Task WaitUntilLateUpdate(this Object context, float delay, CancellationToken cancellationToken)
         {
             return Scheduler.Instance.WaitUntilLateUpdate(delay, cancellationToken, context);
         }
-
+        
+        public static Task WaitUntilLateUpdate(this Object context, float delay)
+        {
+            return Scheduler.Instance.WaitUntilLateUpdate(delay, default, context);
+        }
+        
         public static Task WaitUntilEndOfFrame(this Object context, CancellationToken cancellationToken)
         {
             return Scheduler.Instance.WaitUntilEndOfFrame(cancellationToken, context);
+        }
+        
+        public static Task WaitUntilEndOfFrame(this Object context)
+        {
+            return Scheduler.Instance.WaitUntilEndOfFrame(default, context);
         }
         
         public static Task WaitUntilEndOfFrame(this Object context, int frames, CancellationToken cancellationToken)
@@ -46,9 +81,19 @@ namespace Fizz6.Scheduler
             return Scheduler.Instance.WaitUntilEndOfFrame(frames, cancellationToken, context);
         }
         
+        public static Task WaitUntilEndOfFrame(this Object context, int frames)
+        {
+            return Scheduler.Instance.WaitUntilEndOfFrame(frames, default, context);
+        }
+        
         public static Task WaitUntilEndOfFrame(this Object context, float delay, CancellationToken cancellationToken)
         {
             return Scheduler.Instance.WaitUntilEndOfFrame(delay, cancellationToken, context);
+        }
+        
+        public static Task WaitUntilEndOfFrame(this Object context, float delay)
+        {
+            return Scheduler.Instance.WaitUntilEndOfFrame(delay, default, context);
         }
     }
 }
--- Assets/Tests/Runtime/SchedulerTestInvoker.cs
+++ Assets/Tests/Runtime/SchedulerTestInvoker.cs
@@ -9,7 +9,16 @@ namespace Fizz6.Scheduler.Tests
         {
             var cancellationToken = new CancellationToken();
             await this.WaitUntilUpdate(2.0f, cancellationToken);
-            Debug.LogError("Invoke In Update");
+            Debug.Log("Invoke In Update After 2 Seconds");
+            InvokeInLateUpdateTest();
+            await this.WaitUntilUpdate(1.0f);
+            Debug.Log("Invoke In Update After 1 Second");
+        }
+
+        private async void InvokeInLateUpdateTest()
+        {
+            await this.WaitUntilLateUpdate(1.0f);
+            Debug.Log("Invoke In Late Update After 1 Second");
         }
     }
 }
